7323ab8332889957e463dcf96c4c8a090b8439eb
PauliusTaraskevicius <fixas1029@gmail.com>
2023-03-09 19:53:58 +0200

    working converter

--- components/buttons/button.js
+++ components/buttons/button.js
@@ -1,18 +1,84 @@
+import { ChangeEventHandler, useCallback, useState } from "react";
 import Image from "next/image";
+import * as Helpers from "../../helpers/helpers";
 
 function Button() {
+  const [uploadedImages, setUploadedImages] = useState([]);
+
+  const handleImageUpload = useCallback(
+    function (event) {
+      const fileList = event.target.files;
+      const fileArray = fileList ? Array.from(fileList) : [];
+      const fileToImagesPromises = fileArray.map(Helpers.fileToImageURL);
+
+      Promise.all(fileToImagesPromises).then(setUploadedImages);
+    },
+    [setUploadedImages]
+  );
+
+  const cleanUploadedImages = useCallback(() => {
+    setUploadedImages([]);
+    uploadedImages.forEach((image) => {
+      URL.revokeObjectURL(image.src);
+    });
+  }, [setUploadedImages, uploadedImages]);
+
+  const generatePdfFromImages = useCallback(() => {
+    Helpers.generatePdfFromImages(uploadedImages);
+    cleanUploadedImages();
+  }, [uploadedImages, cleanUploadedImages]);
+
   return (
-    <button className="relative btn-bg-color transition duration-150 ease-in-out font-bold text-sm sm:text-lg md:text-xl lg:text-2xl rounded-full px-24 sm:px-28 md:px-52 lg:px-72 py-6 whitespace-nowrap">
-      <div className="absolute left-0 px-4 py-0.5 sm:px-5 sm:py-1.5 md:px-7 md:py-1.5 xl:px-8 xl:py-2">
-        <Image
-          width="16"
-          height="16"
-          alt="plus"
-          src="/../public/images/plus.png"
-        />
+    <>
+      
+      <div className="images-container">
+        {uploadedImages.length > 0 ? (
+          uploadedImages.map((image) => (
+            <img key={image.src} src={image.src} className="uploaded-image" />
+          ))
+        ) : (
+          <p>Upload some images...</p>
+        )}
       </div>
-      Choose File
-    </button>
+
+      <div className="buttons-container">
+        <label htmlFor="file-input">
+          <span className="button">Upload images</span>
+          <input
+            id="file-input"
+            type="file"
+            accept="image/*"
+            onChange={handleImageUpload}
+            style={{ display: "none" }}
+            multiple
+          />
+        </label>
+
+        <button
+          onClick={generatePdfFromImages}
+          className="button"
+          disabled={uploadedImages.length === 0}
+        >
+          Generate PDF
+        </button>
+      </div>
+
+      {/* <input type="file" id="file-input" hidden />
+      <label
+        htmlFor="file-input"
+        className="relative btn-bg-color transition duration-150 ease-in-out font-bold text-sm sm:text-lg md:text-xl lg:text-2xl rounded-full px-24 sm:px-28 md:px-52 lg:px-72 py-6 whitespace-nowrap hover:cursor-pointer"
+      >
+        <div className="absolute left-0 px-4 py-0.5 sm:px-5 sm:py-1.5 md:px-7 md:py-1.5 xl:px-8 xl:py-2">
+          <Image
+            width="16"
+            height="16"
+            alt="plus"
+            src="/../public/images/plus.png"
+          />
+        </div>
+        Choose File
+      </label> */}
+    </>
   );
 }
 
---
+++ helpers/helpers.js
@@ -0,0 +1,89 @@
+import jsPDF from "jspdf";
+
+const A4_PAPER_DIMENSIONS = {
+  //   width: 210,
+  //   height: 297,
+  width: 612,
+  height: 792,
+};
+
+const A4_PAPER_RATIO = A4_PAPER_DIMENSIONS.width / A4_PAPER_DIMENSIONS.height;
+
+export const imageDimensionsOnA4 = (dimensions) => {
+  const isLandscapeImage = dimensions.width >= dimensions.height;
+
+  if (isLandscapeImage) {
+    return {
+      width: A4_PAPER_DIMENSIONS.width,
+      height:
+        A4_PAPER_DIMENSIONS.width / (dimensions.width / dimensions.height),
+    };
+  }
+
+  const imageRatio = dimensions.width / dimensions.height;
+
+  if (imageRatio > A4_PAPER_RATIO) {
+    const imageScaleFactor =
+      (A4_PAPER_RATIO * dimensions.height) / dimensions.width;
+
+    const scaledImageHeight = A4_PAPER_DIMENSIONS.height * imageScaleFactor;
+
+    return {
+      height: scaledImageHeight,
+      width: scaledImageHeight * imageRatio,
+    };
+  }
+
+  return {
+    width: A4_PAPER_DIMENSIONS.height / (dimensions.height / dimensions.width),
+    height: A4_PAPER_DIMENSIONS.height,
+  };
+};
+
+export const fileToImageURL = (file) => {
+  return new Promise(function (resolve, reject) {
+    const image = new Image(file.type);
+
+    image.onload = () => {
+      resolve(image);
+    };
+
+    image.onerror = () => {
+      reject(new Error("Failed to convert File to Image"));
+    };
+
+    image.src = URL.createObjectURL(file);
+  });
+};
+
+export const generatePdfFromImages = (images) => {
+  const doc = new jsPDF({
+    orientation: "l",
+    unit: "pt",
+    format: [A4_PAPER_DIMENSIONS.width, A4_PAPER_DIMENSIONS.height],
+  });
+
+  doc.deletePage(1);
+
+  images.forEach((image) => {
+    const imageDimensions = imageDimensionsOnA4({
+      width: image.width,
+      height: image.height,
+    });
+
+    doc.addPage();
+    doc.addImage(
+      image.src,
+      image.imageType,
+      A4_PAPER_DIMENSIONS.width * 0.2,
+      A4_PAPER_DIMENSIONS.height * 0.2
+      //   (A4_PAPER_DIMENSIONS.width - imageDimensions.width) / 2,
+      //   (A4_PAPER_DIMENSIONS.height - imageDimensions.height) / 2,
+      //   imageDimensions.width,
+      //   imageDimensions.height
+    );
+  });
+
+  const pdfURL = doc.output("bloburl");
+  window.open(pdfURL, "_blank");
+};
